42fdac21c14261bee99618cf0481c958b94f3787
Vincent ROBERT <33493725+m4th2@users.noreply.github.com>
2022-03-25 08:04:12 +0100

    Ne stocker aucun identifiant sur GitHub

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
 //Fichier intégrant les paramètres de connexion
+//Ne stocker aucun identifiant sur GitHub. Ce fichier sera simplement modifié en local avant d'être envoyé sur le serveur
 $adresse="localhost:3306";
 $identifiant="root";
 $mdp="root";
