4c1f770684ef48750b304a47bc80ae9171fc4f12
Vincent ROBERT <33493725+m4th2@users.noreply.github.com>
2022-04-10 15:26:43 +0200

    Update resultats.php
    On va annoncer les résultats

--- resultats.php
+++ resultats.php
@@ -1,85 +1,101 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
-<html>
+<html lang="fr" prefix="og: http://ogp.me/ns#">
     <head>
-        <title>Concours</title>
+        <title>PixChoice.nsi.xyz</title>
+
+        <!-- META VERSE -->
         <meta charset="utf-8" />
-		
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- OG CARD -->
+        <!--
+            J'ai mis des trucs au pif, faudra le mettre à jour à la fin pour le site.    
+        -->
+        <meta property="og:title" content="pixchoice.nsi.xyz">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://pixchoice.nsi.xyz">
+        <meta property="og:image" content="https://pixchoice.nsi.xyz/icon.jpg">
+        <meta property="og:description" content="Venez votez pour vos images favorites ! Réalisées par des élèves de 2nde du lycée Louis Pasteur Avignon">
+        <meta property="og:site_name" content="nsi.xyz">
+        
+        <!-- LINKS -->
+        <link rel="stylesheet" href="stylesheets/main_style.css">
+        <link rel="shortcut icon" href="icon.jpg">
     </head>
 
-<style>
-body{
-	
-	background-image:url(motif.png);
-	font-family: Verdana, sans-serif;
-}
-.page{
-	background-color:#f9f9f9;
-	margin:1em;
-	padding:1em;
-	box-shadow: 10px 10px 5px 5px rgba(0, 0, 0, 0.2);
-	border-radius:2em;
-}
-.contenu{
-	background-color:#f0f0f0;
-	margin:0em 0.25em 1em 0.25em;
-	padding: 0.5em 1em;
-	border-radius:1em;
-	box-shadow: 5px 5px 2px 2px rgba(0, 0, 0, 0.2);
-}
-.container {
-  display: flex;
-  flex-wrap:wrap;
-  width:100%;
-  justify-content:space-between;
-
-}
-@media (orientation:portrait){
-	.item{
-  
-	  width:30%;
-	  flex-flow: row wrap;
-	  align-items: center;
-	  object-fit: contain;
-	}
-}
-
-@media (orientation:landscape){
-	.item{
-  
-	  width:19%;
-	  flex-flow: row wrap;
-	  align-items: center;
-	  object-fit: contain;
-	}
-}
-
-
-
-img{
-	width:100%;
-}
-/* HIDE RADIO */
-[type=radio] { 
-  position: absolute;
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-
-/* IMAGE STYLES */
-[type=radio] + img {
-  cursor: pointer;
-}
-
-/* CHECKED STYLES */
-[type=radio]:checked + img {
-  outline: 2px solid #f00;
-}
-button {
-	width:100%;
-}
-</style>
 <body>
+        <?php
+            // Include IDs for PDO connexion
+            include("config.php");
+
+            // Create PDO
+            $dsn = 'mysql:host=' . substr($adresse, 1000, -5) . ';dbname=' . $database . ';charset=UTF8';
+            $dbh = new PDO($dsn, $identifiant, $mdp) or die("Pb de connexion !");
+
+            // Do we have to post a vote ?
+            $deja_vote = 0;
+            if (!empty($_POST)){
+                if (isset($_POST["nom_image"]) && strlen($_POST["nom_image"]) == 7){
+                    // Vote for a given image (image in $_POST)
+                    $ajout="UPDATE concours
+                        SET nb_votes = nb_votes + 1
+                        WHERE image = '" . $_POST["nom_image"] . "'";
+
+                    $sth = $dbh -> prepare($ajout);
+                    $sth -> execute();
+
+                    // Add fellow to votelist
+                    $votant_query = "SELECT ip FROM votant";
+                    $sth = $dbh -> prepare($votant_query);
+                    $sth -> execute();
+                    $votant = $sth -> fetchAll();
+
+                    $current_ip = $_SERVER["REMOTE_ADDR"];
+
+                    if (!in_array($current_ip, $votant)) {
+                        $add_votant_query = "INSERT INTO votant VALUES (?)";
+                        $sth = $dbh -> prepare($add_votant_query);
+                        $sth -> execute(array($current_ip));
+                    }
+
+                    foreach ($_SESSION["last_seen"] as $enr) {
+                        // Increment image occurrence number
+                        $ajout="UPDATE concours
+                            SET nb_fois = nb_fois + 1
+                            WHERE id = '" . $enr . "'";
+
+                        $sth = $dbh->prepare($ajout);
+                        $sth->execute();
+                        $result_ajout = $sth->fetchAll();
+                    }
+                }
+                $deja_vote = intval($_POST['deja_vote']);
+            }
+
+            // Get number of votes and people
+            $votes = "SELECT SUM(nb_votes) as result FROM concours";
+            $sth = $dbh -> prepare($votes);
+            $sth -> execute();
+            $votes_actuel = $sth -> fetchAll();
+
+            $people = "SELECT COUNT(ip) FROM votant";
+            $sth = $dbh -> prepare($people);
+            $sth -> execute();
+            $people_actuel = $sth -> fetchAll();
+        ?>
+        
+        <header>
+            <h1>Les mathématiques sont belles, 3<sup>ème</sup> ed. 2022</h1>
+            <p>Votes : <?php echo $votes_actuel[0][0]; ?> | Votants : <?php echo $people_actuel[0][0]; ?></p>
+        </header>
+
+        <article>
+            <p>Les élèves de en classe de seconde 5 et 7 du lycée Louis Pasteur d'Avignon ont produit 55 images uniques sur leur calculatrice NumWorks.</p>
+            <p>Vous pouviez : <b>voter pour vos images favorites,</b> mais le vote est clos. <a href="https://twitter.com/nsi_xyz/status/1507684348820082690">Découvrir une sélection de 42 images</a></p>
+            <p> Cette page présente le résultat du vote</p> 
+        </article>
+
 <?php
 include "config.php";
 $dsn = 'mysql:host='.substr($adresse, 1000, -5).';dbname='.$database.';charset=UTF8';
